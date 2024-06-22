e0ba33ebae1edf31b6a08d190faeb4347eb56f71
kevindlf <kevindlf2004@gmail.com>
2024-06-21 21:05:41 -0400

    Primera parte del inicio de secion

---
+++ src/main/java/com/mycompany/boletosdeavion/BoletosDeAvion.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.boletosdeavion;
+
+import interfaces.InicioDeSesion;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class BoletosDeAvion {
+
+    public static void main(String[] args) {
+        InicioDeSesion inicio = new InicioDeSesion();
+        inicio.setVisible(true);
+       
+    }
+}
